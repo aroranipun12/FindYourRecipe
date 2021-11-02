764a93f704f0c656c7deb52b57d43ddf01e264cf
Nipun Arora <aroranipun1@gmail.com>
2021-11-02 14:02:01 +0530

    implemented pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -33,15 +33,18 @@ const controlSearchResults = async function () {
   try {
     resultsView.renderSpinner();
     //console.log(resultsView);
+
     //1. Get search query
     const query = searchView.getQuery();
     if (!query) return;
+
     //2. Load search results
     await model.loadSearchResults(query);
+
     //3. render search results
     //console.log(model.state.search.results);
     //resultsView.render(model.state.search.results);//all results at once
-    resultsView.render(model.getSearchResultsPage(6));
+    resultsView.render(model.getSearchResultsPage());
 
     //4) REnder initial pagination buttons
     paginationView.render(model.state.search);
@@ -50,8 +53,22 @@ const controlSearchResults = async function () {
   }
 };
 
+const controlPagination = function (goToPage) {
+  //3. render new search results
+  //console.log(model.state.search.results);
+  //resultsView.render(model.state.search.results);//all results at once
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  //4) REnder new pagination buttons
+  paginationView.render(model.state.search);
+
+  //paginationView.render(model.getSearchResultsPage(goToPage));
+  //console.log(goToPage);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -4,10 +4,23 @@ import icons from 'url:../../img/icons.svg';
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+      // console.log(btn); // which we clicked
+      if (!btn) return; // guard clause
+      const goToPage = +btn.dataset.goto; //where we need to go
+      //console.log(goToPage);
+      handler(goToPage);
+    });
+  }
+
   _generateMarkup() {
     const curPage = this._data.page;
     const markup_forward = `
-            <button class="btn--inline pagination__btn--next">
+            <button data-goto="${
+              curPage + 1
+            }" class="btn--inline pagination__btn--next">
                 <span>Page ${curPage + 1}</span>
                 <svg class="search__icon">
                 <use href="${icons}#icon-arrow-right"></use>
@@ -15,7 +28,9 @@ class PaginationView extends View {
             </button>
       `;
     const markup_backward = `
-            <button class="btn--inline      pagination__btn--prev">
+            <button data-goto = "${
+              curPage - 1
+            }" class="btn--inline      pagination__btn--prev">
                 <svg class="search__icon">
                 <use href="${icons}#icon-arrow-left"></use>
                 </svg>
@@ -25,7 +40,7 @@ class PaginationView extends View {
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
-    console.log(numPages);
+    //console.log(numPages);
     //scenarios for pagination
     //Page 1 , and there are other pages
     if (curPage === 1 && numPages > 1) {
