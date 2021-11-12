4c977c8a8bc75ee6088848cf825d3a6f2b8dfa21
aroranipun12 <aroranipun1@gmail.com>
2021-11-12 13:21:49 +0530

    bug fix-> fractional library was causing the bug. So , i have removed fractional as a dependency and coded a function which converts decimal to fraction

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
 import View from './View.js';
+import { numberToFraction } from '../helpers.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -133,7 +133,7 @@ class RecipeView extends View {
                 <use href="${icons}#icon-check"></use>
               </svg>
               <div class="recipe__quantity">${
-                ing.quantity ? new Fraction(ing.quantity).toString() : ' '
+                ing.quantity ? numberToFraction(ing.quantity) : ' '
               }</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
